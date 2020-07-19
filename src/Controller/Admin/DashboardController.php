65e83ee0317bd32fc2549ddf96b10c30e2def093
Michal Piotrowski <michal@eventhorizonlabs.eu>
2020-07-19 12:55:13 +0000

    #10 dashboard statistics p1

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\QuoteRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,8 +16,16 @@ class DashboardController extends AbstractController
     /**
      * @Route("/", name="dashboard")
      */
-    public function index(): Response
-    {
-        return $this->render('admin/dashboard/index.html.twig');
+    public function index(
+        QuoteRepository $quoteRepository,
+        UserRepository $userRepository
+    ): Response {
+        $quotesCount = $quoteRepository->count([]);
+        $usersCount = $userRepository->count([]);
+
+        return $this->render('admin/dashboard/index.html.twig', [
+            'quotes_count' => $quotesCount,
+            'users_count' => $usersCount,
+        ]);
     }
 }
