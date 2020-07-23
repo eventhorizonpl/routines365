406f7176fb3cf67588c65819244ba5717420915a
Michal Piotrowski <michal@eventhorizonlabs.eu>
2020-07-23 19:31:01 +0000

    #17 static pages p1
     #17 static pages p2
    
     #17 static pages p3
    
     #17 static pages p4

---
+++ src/Controller/Frontend/StaticPageController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller\Frontend;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/page", name="frontend_static_page_")
+ */
+class StaticPageController extends AbstractController
+{
+    /**
+     * @Route("/about", name="about")
+     */
+    public function about(): Response
+    {
+        return $this->render('frontend/static_page/about.html.twig');
+    }
+
+    /**
+     * @Route("/disclaimer", name="disclaimer")
+     */
+    public function disclaimer(): Response
+    {
+        return $this->render('frontend/static_page/disclaimer.html.twig');
+    }
+
+    /**
+     * @Route("/privacy-policy", name="privacy_policy")
+     */
+    public function privacyPolicy(): Response
+    {
+        return $this->render('frontend/static_page/privacy_policy.html.twig');
+    }
+
+    /**
+     * @Route("/terms-and-conditions", name="terms_and_conditions")
+     */
+    public function termsAndConditions(): Response
+    {
+        return $this->render('frontend/static_page/terms_and_conditions.html.twig');
+    }
+}
