bff67592bb19a640f1743b51321e9caede87c1cc
Michal Piotrowski <michal@eventhorizonlabs.eu>
2020-10-28 22:09:19 +0100

    #69 chinese proverbs p1

--- src/DataFixtures/QuoteV2Fixtures.php
+++ src/DataFixtures/QuoteV2Fixtures.php
@@ -250,6 +250,230 @@ class QuoteV2Fixtures extends Fixture implements FixtureGroupInterface
                 'author' => 'Confucius',
                 'content' => 'Ignorance is the night of the mind, but a night without moon and star.',
             ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'A crisis is an opportunity riding the dangerous wind.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'It’s better to be without a book than to believe a book entirely.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'A little impatience will spoil great plans.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'A journey of a thousand miles begins with a single step.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'A smile will gain you ten more years of life.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'A bird does not sing because it has an answer. It sings because it has a song.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'A book holds a house of gold.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Talk does not cook rice.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'A man who cannot tolerate small misfortunes can never accomplish great things.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Experience is a comb which nature gives us when we are bald.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Be not afraid of growing slowly, be afraid only of standing still.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'A fall into a ditch makes you wiser.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'He who asks is a fool for five minutes, but he who does not ask remains a fool forever.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'An inch of time is an inch of gold but you can’t buy that inch of time with an inch of gold.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'A closed mind is like a closed book; just a block of wood.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Better to light a candle than to curse the darkness.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'A needle is not sharp at both ends.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Habits are cobwebs at first; cables at last.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'The best time to plant a tree was 20 years ago. The second best time is today.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Patience is a bitter plant, but its fruit is sweet.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Two good talkers are not worth one good listener.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'A hundred no’s are less agonizing than one insincere yes.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'He who cheats the earth will be cheated by the earth.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Only one who can swallow an insult is a man.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'One beam, no matter how big, cannot support an entire house on its own.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Better the cottage where one is merry than the palace where one weeps.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Distant water does not put out a nearby fire.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'He who thinks too much about every step he takes will always stay on one leg.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'All things change, and we change with them.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'If you want to find out about the road ahead, then ask about it from those coming back.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Genius can be recognized by its childish simplicity.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'There are two kinds of perfect people: those who are dead, and those who have not been born yet.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Learning is a weightless treasure you can always carry easily.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Small men think they are small; great men never know they are great.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Do everything at the right time, and one day will seem like three.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Solve one problem, and you keep a hundred others away.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'It is easy to open a store - the hard part is keeping it open.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'To be totally at leisure for one day is to be immortal for one day.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Fool me once, shame on you; fool me twice, shame on me.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Teachers open the door. You enter by yourself.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Ripe fruit falls by itself - but it doesn’t fall in your mouth.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'If you are patient in one moment of anger, you will escape a hundred days of sorrow.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Before preparing to improve the world, first look around your own home three times.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'I was angered, for I had no shoes. Then I met a man who had no feet.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Men trip not on mountains they trip on molehills.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'It is not the knowing that is difficult, but the doing.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'A wise man makes his own decisions, but an ignorant man mindlessly follows the crowd.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'The people who talk the best are not the only ones who can tell you the most interesting things.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Do not believe that you will reach your destination without leaving the shore.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'A clever person turns great troubles into little ones, and little ones into none at all.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'The person who is his own master cannot tolerate another boss.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Guessing is cheap, but guessing wrong can be expensive.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Learning is a treasure that will follow its owner everywhere.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'No matter how tall the mountain is, it cannot block the sun.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Tenacity and adversity are old foes.',
+            ],
+            [
+                'author' => 'Chinese Proverb',
+                'content' => 'Think about your own faults during the first half of the night, and the faults of others during the second half.',
+            ],
         ];
 
         $quotes = [];
