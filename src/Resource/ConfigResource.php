6c32a7cbb983c4fc5c04245ba7dfb1893ddca33f
Michal Piotrowski <michal@eventhorizonlabs.eu>
2020-11-08 13:40:13 +0100

    #63 fix language p1
     #63 fix language p2
    
     #63 fix language p3
    
     #63 fix language p4
    
     #63 fix language p5
    
     #63 fix language p6
    
     #63 fix language p7
    
     #63 fix language p8
    
     #63 fix language p9
    
     #63 fix language p10
    
     #63 fix language p11
    
     #63 fix language p12
    
     #63 fix language p13

--- src/Resource/ConfigResource.php
+++ src/Resource/ConfigResource.php
@@ -7,6 +7,6 @@ class ConfigResource
     public const ACCOUNT_AVAILABLE_EMAIL_NOTIFICATIONS_LIMIT = 2000;
     public const ACCOUNT_AVAILABLE_SMS_NOTIFICATIONS_LIMIT = 200;
     public const COUNTRIES_ALLOWED_FOR_SMS = ['PL', 'US'];
-    public const INVITATIONS_ENABLED = false;
-    public const REGISTRATION_ENABLED = false;
+    public const INVITATIONS_ENABLED = true;
+    public const REGISTRATION_ENABLED = true;
 }
