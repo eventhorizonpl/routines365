6714ebe2a9ea2313ab1312ba8bac4e42dea13186
Michal Piotrowski <michal@eventhorizonlabs.eu>
2020-12-05 23:56:47 +0100

    #194 phpmd

---
+++ bin/phpmd.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ ! -e phpmd.phar ]
+then
+    wget https://github.com/phpmd/phpmd/releases/download/2.9.1/phpmd.phar
+fi
+
+if [ ! -d var/phpmd_report ]
+then
+    mkdir var/phpmd_report
+fi
+
+php phpmd.phar src/ html cleancode, codesize, controversial, design, naming, unusedcode > var/phpmd_report/report.html
