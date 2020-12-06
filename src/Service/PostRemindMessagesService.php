8856a745aec013e75c9bb636e057927cbb8684ac
Michal Piotrowski <michal@eventhorizonlabs.eu>
2020-12-06 11:40:16 +0000

    #165 display in local time format p1

--- src/Service/PostRemindMessagesService.php
+++ src/Service/PostRemindMessagesService.php
@@ -98,7 +98,7 @@ class PostRemindMessagesService
     public function prepareReminderMessages(Reminder $reminder): Reminder
     {
         $message = 'Your routine '.$reminder->getRoutine()->getName();
-        $message .= ' starts at '.$reminder->getNextDateLocalTime()->format('H:i:s').'.';
+        $message .= ' starts at '.$reminder->getRoutineStartDate()->format('H:i:s').'.';
         if ($reminder->getRoutine()->getGoals()->count() > 0) {
             $message .= ' You have '.$reminder->getRoutine()->getGoals()->count().' goals.';
         }
