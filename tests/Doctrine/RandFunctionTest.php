9661bfaeffc159edd55fd9719adeda6896a9d50a
Michal Piotrowski <michal@eventhorizonlabs.eu>
2020-12-27 21:55:50 +0000

    #168 doctrine tests p1
     #168 doctrine tests p2

---
+++ tests/Doctrine/RandFunctionTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Entity;
+
+use App\Doctrine\RandFunction;
+use App\Tests\AbstractTestCase;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+final class RandFunctionTest extends AbstractTestCase
+{
+    public function testConstruct()
+    {
+        $randFunction = new RandFunction('RAND');
+        $this->assertInstanceOf(RandFunction::class, $randFunction);
+    }
+
+    public function testGetSql()
+    {
+        $randFunction = new RandFunction('RAND');
+        $sqlWalker = $this->getMockBuilder(SqlWalker::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->assertIsString($randFunction->getSql($sqlWalker));
+    }
+
+    public function testParse()
+    {
+        $randFunction = new RandFunction('RAND');
+        $parser = $this->getMockBuilder(Parser::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->assertNull($randFunction->parse($parser));
+    }
+}
