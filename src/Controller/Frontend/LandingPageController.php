47d0cdc0172a70c6e6377407d86930cdbfe87f60
Michal Piotrowski <michal@eventhorizonlabs.eu>
2021-01-27 13:54:02 +0000

    #137 landing pages p1
     #137 landing pages p2
    
     #137 landing pages p3
    
     #137 landing pages p4
    
     #137 landing pages p5
    
     #137 landing pages p6
    
     #137 landing pages p7

---
+++ src/Controller/Frontend/LandingPageController.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Frontend;
+
+use App\Entity\Promotion;
+use App\Service\PromotionService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/landing-page", name="frontend_landing_page_")
+ */
+class LandingPageController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('frontend/landing_page/index.html.twig');
+    }
+
+    /**
+     * @Route("/learn-faster", name="learn_faster")
+     */
+    public function learnFaster(): Response
+    {
+        return $this->render('frontend/landing_page/learn_faster.html.twig');
+    }
+
+    public function register(PromotionService $promotionService): Response
+    {
+        $promotion = $promotionService->getEnabledAndValidPromotion(
+            'PLUS10NR',
+            Promotion::TYPE_NEW_ACCOUNT
+        );
+
+        return $this->render('frontend/landing_page/_register.html.twig', [
+            'promotion' => $promotion,
+        ]);
+    }
+}
