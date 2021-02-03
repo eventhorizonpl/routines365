eac00c41efe06171e27d927787e98df66978a912
Michal Piotrowski <michal@eventhorizonlabs.eu>
2021-02-03 13:22:41 +0000

    #312 update Twig tests

---
+++ tests/Twig/ReportExtensionTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Twig;
+
+use App\Tests\AbstractTestCase;
+use App\Twig\ReportExtension;
+
+final class ReportExtensionTest extends AbstractTestCase
+{
+    public function testConstruct(): void
+    {
+        $reportExtension = new ReportExtension();
+
+        $this->assertInstanceOf(ReportExtension::class, $reportExtension);
+    }
+
+    public function testGetFunctions(): void
+    {
+        $reportExtension = new ReportExtension();
+
+        $this->assertCount(2, $reportExtension->getFunctions());
+        $this->assertIsArray($reportExtension->getFunctions());
+    }
+
+    public function testReportStatus(): void
+    {
+        $reportExtension = new ReportExtension();
+
+        $this->assertCount(3, $reportExtension->reportStatus());
+        $this->assertIsArray($reportExtension->reportStatus());
+    }
+
+    public function testReportType(): void
+    {
+        $reportExtension = new ReportExtension();
+
+        $this->assertCount(1, $reportExtension->reportType());
+        $this->assertIsArray($reportExtension->reportType());
+    }
+}
---
+++ tests/Twig/TestimonialExtensionTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Twig;
+
+use App\Tests\AbstractTestCase;
+use App\Twig\TestimonialExtension;
+
+final class TestimonialExtensionTest extends AbstractTestCase
+{
+    public function testConstruct(): void
+    {
+        $testimonialExtension = new TestimonialExtension();
+
+        $this->assertInstanceOf(TestimonialExtension::class, $testimonialExtension);
+    }
+
+    public function testGetFunctions(): void
+    {
+        $testimonialExtension = new TestimonialExtension();
+
+        $this->assertCount(1, $testimonialExtension->getFunctions());
+        $this->assertIsArray($testimonialExtension->getFunctions());
+    }
+
+    public function testTestimonialStatus(): void
+    {
+        $testimonialExtension = new TestimonialExtension();
+
+        $this->assertCount(3, $testimonialExtension->testimonialStatus());
+        $this->assertIsArray($testimonialExtension->testimonialStatus());
+    }
+}
