4d25bb8f8de7ef64172f62bc33d2c411e08b49ef
Michal Piotrowski <michal@eventhorizonlabs.eu>
2021-02-08 17:58:01 +0000

    #323 new version v6

--- bin/load_data_v2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-exit 1
-
-bin/console doctrine:fixtures:load --append --group=v2deployment -n
--- bin/load_data_v5.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-bin/console app:create-retention --today=2020-11-15
-bin/console app:create-retention --today=2020-12-15
-bin/console app:create-retention --today=2021-01-15
-bin/console app:create-retention --today=2021-02-15
