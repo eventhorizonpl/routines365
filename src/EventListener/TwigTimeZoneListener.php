9566b318e480ea8b431c6ca739d542091aa5b8a2
Michal Piotrowski <michal@eventhorizonlabs.eu>
2021-03-08 08:18:49 +0000

    #380 nullsafe p1

--- src/EventListener/TwigTimeZoneListener.php
+++ src/EventListener/TwigTimeZoneListener.php
@@ -27,17 +27,11 @@ class TwigTimeZoneListener
 
     public function onKernelControllerArguments(ControllerArgumentsEvent $event): void
     {
-        if ((null !== $this->security->getUser()) &&
-            (null !== $this->security->getUser()->getProfile()) &&
-            (null !== $this->security->getUser()->getProfile()->getTimeZone())
-        ) {
+        if (null !== $this->security->getUser()?->getProfile()?->getTimeZone()) {
             $timeZone = $this->security->getUser()->getProfile()->getTimeZone();
             $this->environment->getExtension(CoreExtension::class)->setTimeZone($timeZone);
         }
-        if ((null !== $this->security->getUser()) &&
-            (null !== $this->security->getUser()->getProfile()) &&
-            (null !== $this->security->getUser()->getProfile()->getCountry())
-        ) {
+        if (null !== $this->security->getUser()?->getProfile()?->getCountry()) {
             try {
                 $country = (new ISO3166())->alpha2($this->security->getUser()->getProfile()->getCountry());
                 $alpha3 = $country['alpha3'];
