decd8f9d380f61470e3fecbd6f16ad9fb404416d
Michal Piotrowski <michal@eventhorizonlabs.eu>
2021-07-07 16:15:53 +0000

    #439 OpenApi tests p1
     #439 OpenApi tests p2
    
     #439 OpenApi tests p3

---
+++ tests/OpenApi/JwtDecoratorTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\ApiPlatform;
+
+use ApiPlatform\Core\Metadata\Property\{PropertyMetadata, PropertyNameCollection};
+use ApiPlatform\Core\Metadata\Resource\{ResourceMetadata, ResourceNameCollection};
+use ApiPlatform\Core\OpenApi\OpenApi;
+use App\Entity\{Note, User};
+use App\OpenApi\JwtDecorator;
+use App\Tests\AbstractDoctrineTestCase;
+
+/**
+ * @internal
+ */
+final class JwtDecoratorTest extends AbstractDoctrineTestCase
+{
+    /**
+     * @inject
+     */
+    private ?JwtDecorator $jwtDecorator;
+
+    protected function tearDown(): void
+    {
+        $this->jwtDecorator = null;
+
+        parent::tearDown();
+    }
+
+    public function testInvoke(): void
+    {
+        $openApi = $this->jwtDecorator->__invoke();
+
+        $this->assertInstanceOf(OpenApi::class, $openApi);
+    }
+}
