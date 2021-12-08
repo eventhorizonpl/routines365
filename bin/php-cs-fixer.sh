c627d1200f380253e0a2ddb16094ed1650f51cc5
Michal Piotrowski <michal@eventhorizonlabs.eu>
2021-12-08 15:50:35 +0000

    #476 update utilities

--- bin/php-cs-fixer.sh
+++ bin/php-cs-fixer.sh
@@ -2,7 +2,7 @@
 
 if [ ! -e php-cs-fixer.phar ]
 then
-    wget https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v3.2.1/php-cs-fixer.phar
+    wget https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v3.3.2/php-cs-fixer.phar
 fi
 
 php php-cs-fixer.phar fix
--- bin/psalm.sh
+++ bin/psalm.sh
@@ -2,7 +2,7 @@
 
 if [ ! -e psalm.phar ]
 then
-    wget https://github.com/vimeo/psalm/releases/download/4.11.2/psalm.phar
+    wget https://github.com/vimeo/psalm/releases/download/v4.15.0/psalm.phar
 fi
 
 if [ ! -d var/psalm_report ]
